d8b7ae29e0946f4d3508bd2443652ac92fcb8a2e
CamiloCJ09 <camilocampazj@gmail.com>
2021-04-16 17:55:43 -0500

    Recursive algorithm

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+K
+src/model/HanoiProblem.java,a\c\acb0cac5e74d5f0b60b9691a51bbb680e8edcad5
+@
+src/ui/Main.java,e\b\ebe8214aea720e1ed7c01e1c59390b69406ecdc1
---
+++ src/ui/Main.java
@@ -0,0 +1,69 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    private static ArrayList<Integer> firstBar;
+    private  static ArrayList<Integer> secondBar;
+    private static ArrayList<Integer> thirdBar;
+    private static Scanner sc;
+    private int i;
+
+
+    public Main(){
+        firstBar = new ArrayList<>();
+        secondBar = new ArrayList<>();
+        thirdBar = new ArrayList<>();
+        sc = new Scanner(System.in);
+        i = 1;
+    }
+    public static void main(String [] args){
+        Main main = new Main();
+
+        //Save cases
+        int cases = sc.nextInt();
+        ArrayList<Integer> sizes = new ArrayList<>();
+        int size = 0;
+        while(cases > 0){
+            size = sc.nextInt();
+            sizes.add(size);
+            cases--;
+        }
+
+
+        for(int i = 0; i < sizes.size(); i++){
+            main.fillFirstBar(sizes.get(i));
+            Collections.reverse(firstBar);
+            solve(sizes.get(i), firstBar, thirdBar, secondBar);
+            System.out.println();
+            firstBar.clear();
+            secondBar.clear();
+            thirdBar.clear();
+        }
+
+    }
+
+    public void fillFirstBar(int maxNum){
+        for(int i = 0; i < maxNum; i++){
+            firstBar.add(i);
+        }
+    }
+
+    public static void solve(int numOfDisks, ArrayList<Integer> from, ArrayList<Integer> to, ArrayList<Integer> aux){
+        if(numOfDisks == 1){
+            System.out.println(firstBar.size()+" "+ secondBar.size()+" "+thirdBar.size());
+            move(from, to);
+            System.out.println(firstBar.size()+" "+ secondBar.size()+" "+thirdBar.size());
+        }else{
+            solve(numOfDisks-1 ,from, aux, to);
+            move(from, to);
+            solve(numOfDisks-1, aux, to, from);
+        }
+    }
+    public static void move(ArrayList<Integer> from, ArrayList<Integer> to){
+        to.add(from.get(from.size()-1));
+        from.remove(from.size()-1);
+    }
+}
